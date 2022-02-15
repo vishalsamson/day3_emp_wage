8f6bb05be16dd41cfd67562ccfa09fb80b631317
Vishal <vishaljagale942@gmail.com>
2022-02-15 19:02:34 +0530

    added uc3

--- src/emp_wage/Emp_wages.java
+++ src/emp_wage/Emp_wages.java
@@ -2,12 +2,20 @@ package emp_wage;
 
 public class Emp_wages {
 	public static void main(String[] args) {
-		int is_full_time=1;
-		double empcheck=Math.floor(Math.random()*10)%2;
-		if (empcheck==is_full_time)
-			System.out.println("Employee is Present");
+		int is_full_time=2;
+		int is_part_time=1;
+		int emp_rate_per_hour=20;
+		int emphrs=0;
+		int empwage=0;
+		double empcheck=Math.floor(Math.random()*10)%3;
+		if (empcheck==is_part_time)
+			emphrs=4;
+		else if (empcheck==is_full_time)
+			emphrs=8;
 		else
-			System.out.println("Employee is absent");
+			emphrs=0;
+		empwage=emphrs*emp_rate_per_hour;
+		System.out.println("Emp wages :"+empwage);
 			
 		
 	}
