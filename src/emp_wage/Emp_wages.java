c773cfb06182d38928eb21c5fb8e9463594d9362
Vishal <vishaljagale942@gmail.com>
2022-02-15 19:40:21 +0530

    added uc6

--- src/emp_wage/Emp_wages.java
+++ src/emp_wage/Emp_wages.java
@@ -5,10 +5,12 @@ public class Emp_wages {
 	public static final int is_part_time=1;
 	public static final int emp_rate_per_hour=20;
 	public static final int num_of_working_days=20;
+	public static final int max_hrs_in_month=10;
 	
 	public static void main(String[] args) {
-		int emphrs=0,empwage=0, totalempwage=0;
-		for(int day=0; day<num_of_working_days;day++) {
+		int emphrs=0,totalemphrs=0, totalworkingdays=0;
+		while(totalemphrs<=max_hrs_in_month && totalworkingdays<num_of_working_days) {
+			totalworkingdays++;
 			int empcheck= (int) Math.floor(Math.random()*10)%3;
 			switch (empcheck) {
 			case is_part_time:
@@ -21,11 +23,13 @@ public class Emp_wages {
 				emphrs=0;
 
 		    }
-			empwage=emphrs*emp_rate_per_hour;
-			totalempwage+=empwage;
-			System.out.println("EMP wage:"+empwage);
+			totalemphrs+=emphrs;
+			System.out.println("day#: " + totalworkingdays+ "emphrs" + emphrs);
 		}
-		System.out.println("Total emp wage:"+totalempwage);
+		int totalempwage=totalemphrs*emp_rate_per_hour;
+		System.out.println("Total emp wage: "+totalempwage);
+		
+		
 		
 			
 		
